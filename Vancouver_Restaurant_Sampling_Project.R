452ef374c78f0bb6140b4880cee042751c3fff13
Tran Anh Thu Phung <ptat1907@gmail.com>
2024-10-30 16:39:48 -0700

    Add more code for finding sample size

--- Vancouver_Restaurant_Sampling_Project.R
+++ Vancouver_Restaurant_Sampling_Project.R
@@ -1,2 +1,12 @@
-# Load data into R
-data <- read.csv("")
+# 1. Calculate the sample size 
+# a. SRS:
+n <- (1.96)^2*0.25/(0.05)^2
+n <- round(n, 0)
+
+data_size <- nrow(cleaned_full_data)
+
+n_fpc <- round(n/(1+n/data_size), 0)
+# n_fpc is 344
+
+# b. Stratified Sampling
+n_strata = n_fpc/8
