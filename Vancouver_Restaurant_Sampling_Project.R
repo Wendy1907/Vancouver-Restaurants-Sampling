31f31b3ba0685f23430df1aae946562cfdc929e9
Tran Anh Thu Phung <ptat1907@gmail.com>
2024-11-03 16:45:11 -0800

    Finish code for SRS

--- Vancouver_Restaurant_Sampling_Project.R
+++ Vancouver_Restaurant_Sampling_Project.R
@@ -1,12 +1,43 @@
-# 1. Calculate the sample size 
-# a. SRS:
-n <- (1.96)^2*0.25/(0.05)^2
-n <- round(n, 0)
+library(tidyverse)
 
-data_size <- nrow(cleaned_full_data)
+# Data cleaning and data preparation
+full_data <- read.csv("cleaned_full_data.csv", header = T)
+data <- full_data[,c("X", "restaurant", "star", "num_reviews", "city")] %>%
+  rename(id = X)%>% 
+  drop_na()
+data
 
-n_fpc <- round(n/(1+n/data_size), 0)
-# n_fpc is 344
+dim(data)
+summary(data)
 
-# b. Stratified Sampling
-n_strata = n_fpc/8
+# Simple Random Sampling
+# Population mean of star
+p_mean_star <- mean(data$star)
+p_mean_star
+
+IDS <- data$id
+N <- length(data$id)
+n <- 335
+
+srs_sample <- sample.int(N, n, replace = F)
+srs_sample.IDS <- IDS[srs_sample]
+data_srs_sample <- subset(data, id %in% srs_sample.IDS)
+
+dim(data_srs_sample)
+
+# Sample Mean
+srs_sample.mean <- mean(data_srs_sample$star)
+srs_sample.mean
+
+# Sample Variance
+srs_sample.variance <- sum((data_srs_sample$star-srs_sample.mean)^2)/(length(data_srs_sample$star)-1)
+srs_sample.variance
+
+# Sample SD
+srs_sample.sd <- sqrt(srs_sample.variance)
+srs_sample.sd
+
+# Calculate 95% CI for population mean
+srs_sample.se <- sqrt((1-n/N)/n)*srs_sample.sd
+srs_CI <- c(srs_sample.mean - 1.96*srs_sample.se, srs_sample.mean + 1.96*srs_sample.se)
+srs_CI
